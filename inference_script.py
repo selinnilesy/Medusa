dad2daf8394d4ae07106a4133e63ee387346aec5
Yingbing Huang <yh21@login1.hpcfund>
2024-11-19 20:08:30 -0600

    script for running medusa

--- inference_script.py
+++ inference_script.py
@@ -61,7 +61,7 @@ if args.input_file is not None:
             question = pf.read()
 else:
     # self-defined question
-    question = "hiiiiiiiiiiii, how are you"
+    question = "Self-correction is an approach to improving responses from large language models (LLMs) by refining the responses using LLMs during inference. Prior work has proposed various self-correction frameworks using different sources of feedback, including self-evaluation and external feedback. However, there is still no consensus on the question of when LLMs can correct their own mistakes, as recent studies also report negative results. In this work, we critically survey broad papers and discuss the conditions required for successful selfcorrection. We first find that prior studies often do not define their research questions in detail and involve impractical frameworks or unfair evaluations that over-evaluate selfcorrection. To tackle these issues, we categorize research questions in self-correction research and provide a checklist for designing appropriate experiments. Our critical survey based on the newly categorized research questions shows that (1) no prior work demonstrates successful self-correction with feedback from prompted LLMs, except for studies in tasks that are exceptionally suited for self-correction, (2) self-correction works well in tasks that can use reliable external feedback, and (3) large-scale fine-tuning enables self-correction."
 
 # start a new conversation
 conv = get_conversation_template(args.model)
@@ -74,6 +74,7 @@ input_ids = tokenizer.encode(prompt, return_tensors="pt").to(
         model.base_model.device
 )
 input_len = input_ids.shape[1]
+print(input_len)
 
 # start generating with medusa with initialization
 # actual generation start from medusa_model.py line 319
